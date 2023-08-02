e93bb671a5cff87df54a6217423c7b9f34a58f10
karsan99 <karenjsanchez@galileo.edu>
2023-08-01 22:15:30 -0600

    Update lab3-script-evaluacion.sh

--- lab3-script-evaluacion.sh
+++ lab3-script-evaluacion.sh
@@ -1,1 +1,35 @@
+#!/bin/bash
 
+## Este es un archivo para autoevaluar el avance y para corregir la entrega del laboratorio.
+
+clear
+echo "Laboratorio 3 - Revision"
+
+echo "Ud debe correr este script y mostrar el resultado a su Asistente de Laboratorio."
+echo
+
+echo "Ejercicio No.1"
+echo "Revisión de myscript.sh"
+echo "---------------------------------------"
+sudo ./myscript.sh
+echo "---------------------------------------"
+
+echo "Ejercicio No. 2"
+echo "Revisar script que contiene la ruta actual"
+echo "---------------------------------------"
+sudo ./script.sh
+echo "---------------------------------------"
+
+echo
+echo "Ejercicio No. 3"
+echo "revisión de script de sumatoria"
+echo "---------------------------------------"
+sudo bash suma.sh
+echo "---------------------------------------"
+
+echo
+echo "Ejercicio No. 4"
+echo "revisión de script de ciclo while"
+echo "---------------------------------------"
+sudo ./while.sh
+echo "---------------------------------------"
